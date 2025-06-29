e6ae0e986fd1f8db44aab748a84886032ff1f049
Sathya <neelasathya06@gmail.com>
2025-06-29 18:25:58 +0530

    Setting up product Entity

---
+++ src/main/java/com/app/ecom/repository/ProductRespository.java
@@ -0,0 +1,10 @@
+package com.app.ecom.repository;
+
+import com.app.ecom.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+@Repository
+public interface ProductRespository extends JpaRepository<Product, Long> {
+}
